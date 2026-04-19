ba62559f5eacc5a83ff3c1d2a5900fe0e3133d7a
Kirill Kondrenko <kondrenkokp@yandex.ru>
2026-04-19 18:43:44 +0700

    Refactore

--- internal/sstable/iterator.go
+++ internal/sstable/iterator.go
@@ -10,53 +10,60 @@ type Iterator struct {
 	currentBlockLastKeyIndex int64
 }
 
-func NewIterator(reader *Reader, startIndex, endIndex, startBlockIndex int64) *Iterator {
-	var currentBlockLastKeyIndex int64
-
-	if startBlockIndex < 0 {
-		// Чтобы позволить подавать currentBlockLastKeyIndex=-1 для крайнего случая, когда 0 блоков
-		currentBlockLastKeyIndex = NoIndex
-	} else {
-		currentBlockLastKeyIndex = reader.blocksInfo[startBlockIndex].lastKeyIndex
+func EmptyIterator() *Iterator {
+	return &Iterator{
+		reader:                   nil,
+		currentIndex:             NoIndex,
+		endIndex:                 NoIndex,
+		currentBlockIndex:        NoIndex,
+		currentBlockLastKeyIndex: NoIndex,
 	}
+}
 
+func NewIterator(reader *Reader, startIndex, endIndex, startBlockIndex int64) *Iterator {
 	return &Iterator{
 		reader:                   reader,
 		currentIndex:             startIndex,
 		endIndex:                 endIndex,
-		currentBlockLastKeyIndex: currentBlockLastKeyIndex,
+		currentBlockLastKeyIndex: reader.getBlock(startBlockIndex).lastKeyIndex,
 		currentBlockIndex:        startBlockIndex,
 	}
 }
 
 func (it *Iterator) Next() (key, value []byte, ok bool, err error) {
-	if it.currentIndex == NoIndex || it.currentIndex == it.endIndex {
+	if it.currentIndex == it.endIndex {
 		return nil, nil, false, nil
 	}
 
-	key, value, nextIndex, err := it.reader.readRecord(it.currentIndex, true)
+	var nextIndex int64
+	key, value, nextIndex, err = it.reader.readRecord(it.currentIndex, true)
 	if err != nil {
 		return nil, nil, false, fmt.Errorf("sstable iterator next: failed to read record: %w", err)
 	}
+	ok = true
 
-	if it.currentIndex == it.currentBlockLastKeyIndex {
-		// текущий блок не последний
-		if it.currentBlockIndex+1 < int64(len(it.reader.blocksInfo)) {
-			it.currentBlockIndex++
-			currentBlock := it.reader.blocksInfo[it.currentBlockIndex]
-			it.currentIndex = currentBlock.firstKeyIndex
-			it.currentBlockLastKeyIndex = currentBlock.lastKeyIndex
-		} else {
-			it.currentIndex = NoIndex // дальше блоков нет
-		}
-	} else {
+	if it.currentIndex != it.currentBlockLastKeyIndex {
+		// В текущем блоке ещё есть непрочитанные записи
 		it.currentIndex = nextIndex
+		return
 	}
 
-	return key, value, true, nil
+	if it.currentBlockIndex+1 >= int64(len(it.reader.blocksInfo)) {
+		// Текущий блок последний (дальше блоков нет)
+		it.currentIndex = NoIndex
+		return
+	}
+
+	// Инициализируем чтение записей из следующего блока
+	it.currentBlockIndex++
+	currentBlock := it.reader.getBlock(it.currentBlockIndex)
+	it.currentIndex = currentBlock.firstKeyIndex
+	it.currentBlockLastKeyIndex = currentBlock.lastKeyIndex
+	return
 }
 
 func (it *Iterator) Close() error {
 	it.currentIndex = NoIndex
+	it.endIndex = NoIndex
 	return nil
 }
--- internal/sstable/reader.go
+++ internal/sstable/reader.go
@@ -46,158 +46,8 @@ func NewReader(ioReader io.ReaderAt, totalSize int64) (*Reader, error) {
 	return reader, nil
 }
 
-func (r *Reader) readFooter() error {
-	checkSumLength, _, err := r.readUint32(r.getChecksumLengthOffset())
-	if err != nil {
-		return fmt.Errorf("sstable readFooter: reading checksum length: %w", err)
-	}
-
-	checksum, _, err := r.readBytes(r.getChecksumOffset(int64(checkSumLength)), int(checkSumLength))
-	if err != nil {
-		return fmt.Errorf("sstable readFooter: reading checksum: %w", err)
-	}
-
-	r.footer = &footer{checksum: checksum}
-
-	return nil
-}
-
-func (r *Reader) readAllBlocksInfo() error {
-	blocksNumberUint, _, err := r.readUint64(r.getBlocksNumberOffset())
-	if err != nil {
-		return fmt.Errorf("sstable readAllBlocksInfo: reading blocks number: %w", err)
-	}
-
-	blocksNumber := int64(blocksNumberUint)
-
-	var i int64
-	var offset int64
-
-	offset = r.getBlockInfoOffset(0, blocksNumber)
-	for i = 0; i < int64(blocksNumberUint); i++ {
-		info, off, err := r.readBlockInfo(offset)
-		if err != nil {
-			return fmt.Errorf("sstable readAllBlocksInfo: reading block info %d: %w", i, err)
-		}
-
-		offset = off
-		r.blocksInfo = append(r.blocksInfo, info)
-	}
-
-	return nil
-}
-
-func (r *Reader) readBlockInfo(blockOffset int64) (*readerBlockInfo, int64, error) {
-	firstKeyIndex, off2, err := r.readUint64(blockOffset)
-	if err != nil {
-		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block first key index: %w", err)
-	}
-
-	lastKeyIndex, off3, err := r.readUint64(off2)
-	if err != nil {
-		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block last key index: %w", err)
-	}
-
-	firstKey, _, _, err := r.readRecord(int64(firstKeyIndex), false)
-	if err != nil {
-		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block first key: %w", err)
-	}
-
-	lastKey, _, _, err := r.readRecord(int64(lastKeyIndex), false)
-	if err != nil {
-		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block last key: %w", err)
-	}
-
-	return &readerBlockInfo{
-		firstKey:      firstKey,
-		lastKey:       lastKey,
-		firstKeyIndex: int64(firstKeyIndex),
-		lastKeyIndex:  int64(lastKeyIndex),
-	}, off3, nil
-}
-
-func (r *Reader) getChecksumLengthOffset() int64 {
-	return r.totalSize - 4
-}
-
-func (r *Reader) getChecksumOffset(checksumLength int64) int64 {
-	return r.getChecksumLengthOffset() - checksumLength
-}
-
-func (r *Reader) getBlocksNumberOffset() int64 {
-	return r.getChecksumOffset(int64(len(r.footer.checksum))) - 8
-}
-
-func (r *Reader) getBlockInfoOffset(blockIndex int64, blocksNumber int64) int64 {
-	return r.getBlocksNumberOffset() - 16*(blocksNumber-blockIndex)
-}
-
-func (r *Reader) readRecord(offset int64, readValue bool) ([]byte, []byte, int64, error) {
-	keyLength, off1, err := r.readUint32(offset)
-	if err != nil {
-		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read key length: %w", err)
-	}
-
-	valueLength, off2, err := r.readUint32(off1)
-	if err != nil {
-		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read value length: %w", err)
-	}
-
-	key, off3, err := r.readBytes(off2, int(keyLength))
-	if err != nil {
-		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read key: %w", err)
-	}
-
-	if readValue {
-		value, off4, err := r.readBytes(off3, int(valueLength))
-		if err != nil {
-			return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read value: %w", err)
-		}
-
-		return key, value, off4, nil
-	}
-
-	return key, nil, off3, nil
-}
-
-func (r *Reader) readUint32(offset int64) (uint32, int64, error) {
-	buffer, newOffset, err := r.readBytes(offset, 4)
-
-	if err != nil {
-		return 0, newOffset, fmt.Errorf("sstable readUint32: reading bytes: %w", err)
-	}
-
-	return ByteOrder.Uint32(buffer), newOffset, nil
-}
-
-func (r *Reader) readUint64(offset int64) (uint64, int64, error) {
-	buffer, newOffset, err := r.readBytes(offset, 8)
-
-	if err != nil {
-		return 0, newOffset, fmt.Errorf("sstable readUint64: reading bytes: %w", err)
-	}
-
-	return ByteOrder.Uint64(buffer), newOffset, nil
-}
-
-func (r *Reader) readBytes(offset int64, count int) ([]byte, int64, error) {
-	buffer := make([]byte, count)
-
-	n, err := r.ioReader.ReadAt(buffer, offset)
-	if err != nil {
-		return nil, 0, fmt.Errorf("sstable readBytes: %w", err)
-	}
-	if n < count {
-		return nil, 0, fmt.Errorf("sstable readBytes: %d < %d", n, count)
-	}
-
-	return buffer, offset + int64(count), nil
-}
-
-// Iterator возвращает упорядоченную итерацию по диапазону [start, end).
-// Использует Sparse Index, чтобы найти нужный блок данных.
 func (r *Reader) Iterator(start []byte, end []byte) (*Iterator, error) {
-	emptyIterator := NewIterator(r, NoIndex, NoIndex, NoIndex)
+	emptyIterator := EmptyIterator()
 
 	if len(r.blocksInfo) == 0 {
 		return emptyIterator, nil
@@ -350,6 +200,154 @@ func (r *Reader) Iterator(start []byte, end []byte) (*Iterator, error) {
 	return NewIterator(r, startIndex, endIndex, startBlockIndex), nil
 }
 
+func (r *Reader) readFooter() error {
+	checkSumLength, _, err := r.readUint32(r.getChecksumLengthOffset())
+	if err != nil {
+		return fmt.Errorf("sstable readFooter: reading checksum length: %w", err)
+	}
+
+	checksum, _, err := r.readBytes(r.getChecksumOffset(int64(checkSumLength)), int(checkSumLength))
+	if err != nil {
+		return fmt.Errorf("sstable readFooter: reading checksum: %w", err)
+	}
+
+	r.footer = &footer{checksum: checksum}
+
+	return nil
+}
+
+func (r *Reader) readAllBlocksInfo() error {
+	blocksNumberUint, _, err := r.readUint64(r.getBlocksNumberOffset())
+	if err != nil {
+		return fmt.Errorf("sstable readAllBlocksInfo: reading blocks number: %w", err)
+	}
+
+	blocksNumber := int64(blocksNumberUint)
+
+	var i int64
+	var offset int64
+
+	offset = r.getBlockInfoOffset(0, blocksNumber)
+	for i = 0; i < int64(blocksNumberUint); i++ {
+		info, off, err := r.readBlockInfo(offset)
+		if err != nil {
+			return fmt.Errorf("sstable readAllBlocksInfo: reading block info %d: %w", i, err)
+		}
+
+		offset = off
+		r.blocksInfo = append(r.blocksInfo, info)
+	}
+
+	return nil
+}
+
+func (r *Reader) readBlockInfo(blockOffset int64) (*readerBlockInfo, int64, error) {
+	firstKeyIndex, off2, err := r.readUint64(blockOffset)
+	if err != nil {
+		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block first key index: %w", err)
+	}
+
+	lastKeyIndex, off3, err := r.readUint64(off2)
+	if err != nil {
+		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block last key index: %w", err)
+	}
+
+	firstKey, _, _, err := r.readRecord(int64(firstKeyIndex), false)
+	if err != nil {
+		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block first key: %w", err)
+	}
+
+	lastKey, _, _, err := r.readRecord(int64(lastKeyIndex), false)
+	if err != nil {
+		return nil, 0, fmt.Errorf("sstable readBlockInfo: failed to read block last key: %w", err)
+	}
+
+	return &readerBlockInfo{
+		firstKey:      firstKey,
+		lastKey:       lastKey,
+		firstKeyIndex: int64(firstKeyIndex),
+		lastKeyIndex:  int64(lastKeyIndex),
+	}, off3, nil
+}
+
+func (r *Reader) getChecksumLengthOffset() int64 {
+	return r.totalSize - 4
+}
+
+func (r *Reader) getChecksumOffset(checksumLength int64) int64 {
+	return r.getChecksumLengthOffset() - checksumLength
+}
+
+func (r *Reader) getBlocksNumberOffset() int64 {
+	return r.getChecksumOffset(int64(len(r.footer.checksum))) - 8
+}
+
+func (r *Reader) getBlockInfoOffset(blockIndex int64, blocksNumber int64) int64 {
+	return r.getBlocksNumberOffset() - 16*(blocksNumber-blockIndex)
+}
+
+func (r *Reader) readRecord(offset int64, readValue bool) ([]byte, []byte, int64, error) {
+	keyLength, off1, err := r.readUint32(offset)
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read key length: %w", err)
+	}
+
+	valueLength, off2, err := r.readUint32(off1)
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read value length: %w", err)
+	}
+
+	key, off3, err := r.readBytes(off2, int(keyLength))
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read key: %w", err)
+	}
+
+	if readValue {
+		value, off4, err := r.readBytes(off3, int(valueLength))
+		if err != nil {
+			return nil, nil, 0, fmt.Errorf("sstable readRecord: failed to read value: %w", err)
+		}
+
+		return key, value, off4, nil
+	}
+
+	return key, nil, off3, nil
+}
+
+func (r *Reader) readUint32(offset int64) (uint32, int64, error) {
+	buffer, newOffset, err := r.readBytes(offset, 4)
+
+	if err != nil {
+		return 0, newOffset, fmt.Errorf("sstable readUint32: reading bytes: %w", err)
+	}
+
+	return ByteOrder.Uint32(buffer), newOffset, nil
+}
+
+func (r *Reader) readUint64(offset int64) (uint64, int64, error) {
+	buffer, newOffset, err := r.readBytes(offset, 8)
+
+	if err != nil {
+		return 0, newOffset, fmt.Errorf("sstable readUint64: reading bytes: %w", err)
+	}
+
+	return ByteOrder.Uint64(buffer), newOffset, nil
+}
+
+func (r *Reader) readBytes(offset int64, count int) ([]byte, int64, error) {
+	buffer := make([]byte, count)
+
+	n, err := r.ioReader.ReadAt(buffer, offset)
+	if err != nil {
+		return nil, 0, fmt.Errorf("sstable readBytes: %w", err)
+	}
+	if n < count {
+		return nil, 0, fmt.Errorf("sstable readBytes: %d < %d", n, count)
+	}
+
+	return buffer, offset + int64(count), nil
+}
+
 func (r *Reader) findFirstGreaterOrEqual(blockIndex int, target []byte) (int64, error) {
 	block := r.blocksInfo[blockIndex]
 	var offset = block.firstKeyIndex
@@ -372,4 +370,8 @@ func (r *Reader) findFirstGreaterOrEqual(blockIndex int, target []byte) (int64, 
 	}
 
 	return NoIndex, nil
+}
+
+func (r *Reader) getBlock(i int64) *readerBlockInfo {
+	return r.blocksInfo[i]
 }
